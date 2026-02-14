026e8a0cadbc3882bd6091097bcc30382e21ac99
imralfyyv <126400983+imralfyyv@users.noreply.github.com>
2026-02-14 08:39:28 +0800

    Updated reacts colors

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,15 +2,16 @@
 
 import Aurora from "@/components/Aurora"; 
 import LogoLoop from '@/components/LogoLoop';
-import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss } from 'react-icons/si';
+import { SiReact, SiNextdotjs, SiTypescript, SiTailwindcss, SiFigma} from 'react-icons/si';
 
 export default function Home() {
   // Move your data inside the Home component
   const techLogos = [
-    { node: <SiReact />, title: "React", href: "https://react.dev" },
-    { node: <SiNextdotjs />, title: "Next.js", href: "https://nextjs.org" },
-    { node: <SiTypescript />, title: "TypeScript", href: "https://www.typescriptlang.org" },
-    { node: <SiTailwindcss />, title: "Tailwind CSS", href: "https://tailwindcss.com" },
+    { node: <SiReact color="#61DAFB"/>, title: "React", href: "https://react.dev" },
+    { node: <SiNextdotjs color="#FFFFFF"/>, title: "Next.js", href: "https://nextjs.org" },
+    { node: <SiTypescript color="#3178C6"/>, title: "TypeScript", href: "https://www.typescriptlang.org" },
+    { node: <SiTailwindcss color="#06B6D4"/>, title: "Tailwind CSS", href: "https://tailwindcss.com" },
+    { node: <SiFigma color="#f21ef2" />, title: "Figma", href: "https://www.figma.com" },
   ];
 
   return (
@@ -31,14 +32,14 @@ export default function Home() {
         justifyContent: 'center', 
         textAlign: 'center',      
         padding: '20px',
-        zIndex: 10 
+        zIndex: 20
       }}>
         <h1 style={{ color: 'white', fontSize: 'clamp(2rem, 8vw, 4rem)', margin: 0 }}>
           Hi! This is a testing page.
         </h1>
         
         {/* The LogoLoop Section */}
-        <div style={{ width: '100%', maxWidth: '800px', marginTop: '40px', height: '100px' }}>
+        <div style={{ width: '110%', maxWidth: '3000px', marginTop: '40px', height: '100px' }}>
           <LogoLoop
             logos={techLogos}
             speed={120}
@@ -47,11 +48,13 @@ export default function Home() {
             gap={40}
             hoverSpeed={30}
             scaleOnHover
+            fadeOut
+            fadeOutColor="#ffffff2f"
             ariaLabel="Technology partners"
           />
         </div>
 
-        <p style={{ color: 'rgb(255, 255, 255)', marginTop: '20px' }}>
+        <p style={{ color: 'rgba(255, 255, 255, 0.82)', marginTop: '20px' }}>
           Work in progress...
         </p>
       </div>
